0b426423ef57b615ad3e64d631975f36a6a2dbb4
Mostafa rayan <mr3309296@gmail.com>
2025-02-26 21:43:26 +0200

    add contact page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,14 +1,11 @@
 import FilterProjects from "@/_components/Projects/FilterProjects";
 import ImportantSec from "@/_components/Projects/importanPro/ImportantSec";
 
-const page = async ({
-  searchParams,
-}: {
-  searchParams: { category?: string };
-}) => {
-  const category = searchParams.category || "All";
+const page = ({ searchParams }: { searchParams?: { category?: string } }) => {
+  const category = searchParams?.category || "All";
+
   return (
-    <section className="mt-[140px!important] !mb-10 space-y-14  lg:space-y-20 container">
+    <section className="mt-[140px!important] !mb-10 space-y-14 lg:space-y-20 container">
       <ImportantSec />
       <FilterProjects category={category} />
     </section>
